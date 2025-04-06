d235a3338720b2d74f1a6c849e1adfcb7b71ad17
Dani500-code <danielavbaezligero@gmail.com>
2025-04-06 16:53:16 -0300

    Desafío 2 .py

--- Desafío 2 .py
+++ Desafío 2 .py
@@ -1,11 +1,14 @@
 #Desafio 2#
+#Valores predefinidos
 
-def areaRectangulo(a, b):
-    area = a*b 
-    return area
+largo = 6
+ancho = 8
 
-x = float(input("Ingrese ancho:"))
-y = float(input("Ingrese altura:"))
-resultadoA = areaRectangulo(x,y)
-print("El area es:", resultadoA)
+#Calculo área del rectángulo 
+área = largo * ancho 
 
+#Resultado
+print(f"El área de un rectángulo se define con un largo {largo} y un ancho {ancho} que da como resultado:")
+print(f"Área = {área} al cuadrado.")
+
+Área = 48 al cuadrado 
