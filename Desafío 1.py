4a906dfae968d3419c0ddae889a41374a51ca19f
Dani500-code <danielavbaezligero@gmail.com>
2025-04-06 11:41:41 -0300

    Add files via upload

---
+++ Desafío 1.py
@@ -0,0 +1,7 @@
+
+numero1 = 8
+numero2 = 4
+
+result = numero1 + numero2 
+
+print(f"La suma de {numero1} y {numero2} da como resultado {result}")
---
+++ Desafío 2 .py
@@ -0,0 +1,11 @@
+#Desafio 2#
+
+def areaRectangulo(a, b):
+    area = a*b 
+    return area
+
+x = float(input("Ingrese ancho:"))
+y = float(input("Ingrese altura:"))
+resultadoA = areaRectangulo(x,y)
+print("El area es:", resultadoA)
+
